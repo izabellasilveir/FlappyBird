c8affba5a50b158644ec39b2baa861c12df79a15
unknown <izabellasilveir@gmail.com>
2020-10-05 06:03:45 -0300

    jogo Flappy Bird

---
+++ Assets/Scripts/PipeController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PipeController : MonoBehaviour
+{
+    public Transform pipe;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(gameObject, 3);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += Vector3.right * -2.5f * Time.deltaTime;
+    }
+}
